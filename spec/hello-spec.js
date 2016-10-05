c6b3fce013382ff0837802e34b5e17c5e966004e
Peng.Li <nowind_lee@qq.com>
2016-10-05 19:30:40 +0800

    contain.text demo

--- spec/hello-spec.js
+++ spec/hello-spec.js
@@ -3,34 +3,25 @@
 import React from 'react';
 import Hello from '../src/hello.jsx';
 import chai from 'chai';
+import chaiEnzyme from 'chai-enzyme'
 import spies  from 'chai-spies';
-import {shallow} from 'enzyme';
+import {shallow, mount} from 'enzyme';
+import jsdomGlobal from 'jsdom-global';
+
+jsdomGlobal();
 
 chai.should();
 chai.use(spies);
+chai.use(chaiEnzyme());
 
 describe('<Hello />', () => {
 
-  it('renders to html including inner components', () => {
-    const wrapper = shallow(<Hello fruit={['AAA', 'BBB']}/>);
-    wrapper.html().should.equal("<div><h1>Hello, I like:</h1><div>Box: AAA</div><div>Box: BBB</div></div>");
-  });
-
   it('shows fruit names', () => {
-    const wrapper = shallow(<Hello fruit={['AAA', 'BBB']}/>);
-    // can find text in the component itself
-    wrapper.contains("Hello, I like:").should.be.true;
-    // but can't find it in the inner components
-    // Notice `false`
-    wrapper.contains('Box: AAA').should.be.false;
+    const wrapper = mount(<Hello fruit={['AAA', 'BBB']}/>);
+    wrapper.should.contain.text("Hello, I like:")
+    // Notice: only `mount` allow to check text in inner components
+    // `shallow` doesn't not
+    wrapper.should.contain.text('Box: AAA')
   });
 
-  it('calls onFruitDelete if deletes a fruit', () => {
-    const spy = chai.spy();
-    const wrapper = shallow(<Hello fruit={['AAA', 'BBB']} onDeleteFruit={spy}/>);
-    wrapper.find('button[title="delete"]').first().simulate('click');
-    // can't interact with inner components
-    // Notice `not.have`
-    spy.should.not.have.been.called.with('AAA');
-  })
 });
